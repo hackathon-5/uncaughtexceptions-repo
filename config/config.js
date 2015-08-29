f59ef9c6389bc8080efdf7d93d2be1ea9ab6462c
Sean Lindo <seanlindo@Seans-MacBook-Pro.local>
2015-08-29 00:56:42 -0400

    Serving static files

--- config/config.js
+++ config/config.js
@@ -1,11 +1,14 @@
 var bodyParser = require('body-parser'),
-    morgan = require('morgan');
+    morgan = require('morgan'),
+    express = require('express'),
+    path = require('path');
 
 module.exports = function(app) {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
-  //app.use(methodOverride());
   app.use(morgan('dev'));
-  app.set('views', [ '../server/views' ]);
-  app.set('view engine', 'hjs');
+  app.set('views', [ './server/views' ]);
+  app.set('view engine', 'ejs');
+  app.use(express.static(path.join(__dirname, '../bower_components')));
+  app.use(express.static(path.join(__dirname, '../public')));
 };
--- server/routes/public/index.js
+++ server/routes/public/index.js
@@ -2,13 +2,10 @@ var express = require('express'),
     router = express.Router();
 
 module.exports = function(app) {
-  //router.post('/', candidate_controller.document.addResume);
 
   router.get('/', function(req, res, next) {
-    res.send('hello world');
+    res.render('index');
   });
 
   app.use(router);
-
-  //app.use('/app/v1/candidate/:user_id/documents/resume', router);
 };
