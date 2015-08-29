bf9a2d0459b12268bf4803233966094bcd28bccb
Sean Lindo <seanlindo@Seans-MacBook-Pro.local>
2015-08-29 00:27:04 -0400

    Stashing bing service

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express'),
     app = express(),
     port = 8000;
 
-global.reqlib = require('app-root-path').require;
+global.req = require('app-root-path').require;
 
 //configure express options
 require('./config/config.js')(app);
